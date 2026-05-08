bef24a197cbcd87403ea219c3fe98b0d3a73db09
Gustavo Donadello <gustavodonadello@hotmail.com>
2026-05-07 21:04:31 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from app.models.database import init_db
+from app.controllers.controllers import filmes_bp, sessoes_bp, publico_bp, cinemas_bp
+
+def create_app():
+    app = Flask(__name__)
+    init_db()
+    app.register_blueprint(filmes_bp)
+    app.register_blueprint(sessoes_bp)
+    app.register_blueprint(publico_bp)
+    app.register_blueprint(cinemas_bp)
+    return app
 
